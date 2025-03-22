ff34f67115846594e2aaf3b9b5c63ff66cbd3ca8
Amruta Janbhor <amruta.janbhor@gmail.com>
2025-03-23 02:23:03 +0530

    Delete users directory

--- users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "full_name", "graduation_year", "current_job", "company", "linkedin"]
